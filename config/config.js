82ddf635a2a9635ec78ba37aef0382cc51df1ac8
lordjoo <youssef.afify122@gmail.com>
2021-01-13 16:32:09 +0200

    hi

--- config/config.js
+++ config/config.js
@@ -14,7 +14,7 @@ var config = {
 							// - another specific IPv4/6 to listen on a specific interface
 							// - "0.0.0.0", "::" to listen on any interface
 							// Default, when address config is left out or empty, is "localhost"
-	port: 8080,
+	port: 5000,
 	basePath: "/", 	// The URL path where MagicMirror is hosted. If you are using a Reverse proxy
 					// you must set the sub path here. basePath must end with a /
 	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], 	// Set [] to allow all IP addresses
@@ -39,72 +39,103 @@ var config = {
 
 	modules: [
 		{
-			module: "clock",
-			position: "top_left",
-			config:{
-			displayType:"analog",
-			analogFace:"face-008",
-		}
-		},
-		{
-			module: "calendar",
-			header: "Egypt Holidays",
-			position: "top_left",
+			module: "MMM-Voice-Commands",
 			config: {
-				fetchInterval:10000, // in ms 
-				calendars: [
-					{
-						symbol: "calendar-check",
-						// https://www.calendarlabs.com/ical-calendar/ics/44/Egypt_Holidays.ics
-						url: "webcal://www.calendarlabs.com/ical-calendar/ics/44/Egypt_Holidays.ics"				
-					},
-					{
-						symbol: "calendar-check",
-						// Enter your private cal url 
-						url: "https://calendar.google.com/calendar/ical/esraa.abdelhakiim%40gmail.com/private-ba7a1a6116b6d3abef41cd22baebaa0d/basic.ics"					
-					}		
-				]
+				debug: true, //Displays end results and errors from annyang in the Log
+				autoStart: true, //Adds annyang commands when it first starts
+				activateCommand: "hello mirror", //Command to active all other commands
+				deactivateCommand: "goodbye mirror", //Command to deactivate all other commands
+				alertHeard: true, //Whether an alert should be shown when annyang hears a phrase (mostly for debug)
+				commands: {
+					"command statement :variable (optional statement)": "SOCKET_NOTIFICATION_NAME",
+					//The payload of the socket notification will be whatever is said in the :variable
+					"command statement *variable": function(param){
+						alert("Whatever is said in the *variable space is given as the "+param);
+						//These function's 'this' are bound to the module's 'this' so you can do stuff like:
+						this.sendNotification("PAGE_SELECT", "2");
+					}
+				}
 			}
 		},
-		{
-			module: "currentweather",
-			position: "top_right",
-			config: {
-				location: "cairo",
-				locationID: "360630", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-				appid: "54b85fab6af9dd58af1f45a1f31b54ef"
-			}
-		},
-		{
-			module: "weatherforecast",
-			position: "top_right",
-			header: "Weather Forecast",
-			config: {
-				location: "cairo",
-				locationID: "360630", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-				appid: "54b85fab6af9dd58af1f45a1f31b54ef"
-			}
-		},
-
 		{
 			module: "MMM-Spotify",
 			position: "top_left",
 			config: {
 			  debug: true,
-			  showDeviceButton:false,
+			  showDeviceButton: true,
 			  style: "mini", // "default", "mini" available.
-			  control: "hidden", // "default" or "hidden"
+			  //control: "hidden", // "default" or "hidden"
 			  moduleWidth: 300, // width of the module in px
 			  miniBarConfig: {
 				album: false, // display Album name in miniBar style
 				scroll: false, // scroll title / artist / album in miniBar style
 				logo: false, // display Spotify logo in miniBar style
+			  },
+			  onStart: {
+				deviceName: "My Mirror", //if null, current(last) activated device will be.
+				spotifyUri: "spotify:track:3ENXjRhFPkH8YSH3qBXTfQ",
+				//when search is set, sportifyUri will be ignored.
+				search: {
+				  type: "album", // `artist`, track`, `album`, `playlist` and its combination(`artist,playlist,album`) be available
+				  keyword: "lovely Billie eilish",
+				  random:true,
+				}
 			  }
 			}
-		  },
-		  {
+		},
+
+		// {
+		// 	module: "clock",
+		// 	position: "top_left",
+		// 	config:{
+		// 	displayType:"analog",
+		// 	analogFace:"face-008",
+		// }
+		// },
+		// {
+		// 	module: "calendar",
+		// 	header: "Egypt Holidays",
+		// 	position: "top_left",
+		// 	config: {
+		// 		fetchInterval:10000, // in ms 
+		// 		calendars: [
+		// 			{
+		// 				symbol: "calendar-check",
+		// 				// https://www.calendarlabs.com/ical-calendar/ics/44/Egypt_Holidays.ics
+		// 				url: "webcal://www.calendarlabs.com/ical-calendar/ics/44/Egypt_Holidays.ics"				
+		// 			},
+		// 			{
+		// 				symbol: "calendar-check",
+		// 				// Enter your private cal url 
+		// 				url: "https://calendar.google.com/calendar/ical/esraa.abdelhakiim%40gmail.com/private-ba7a1a6116b6d3abef41cd22baebaa0d/basic.ics"					
+		// 			}		
+		// 		]
+		// 	}
+		// },
+		// {
+		// 	module: "currentweather",
+		// 	position: "top_right",
+		// 	config: {
+		// 		location: "cairo",
+		// 		locationID: "360630", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+		// 		appid: "54b85fab6af9dd58af1f45a1f31b54ef"
+		// 	}
+		// },
+		// {
+		// 	module: "weatherforecast",
+		// 	position: "top_right",
+		// 	header: "Weather Forecast",
+		// 	config: {
+		// 		location: "cairo",
+		// 		locationID: "360630", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+		// 		appid: "54b85fab6af9dd58af1f45a1f31b54ef"
+		// 	}
+		// },
+
+
+		{
 			module: "newsfeed",
-			position: "top_left",
+			position: "bottom",
 			config: {
 				feeds: [
 					{
